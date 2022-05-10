319aab30094e2a440a97e83328d393b0196ba6ee
Valeria Piont <iceflame.vvv@gmail.com>
2022-05-11 01:15:08 +0300

    stage-4: removed redundant properties

--- src/antifraud/dto/UserAccessDTO.java
+++ src/antifraud/dto/UserAccessDTO.java
@@ -1,8 +1,11 @@
-package antifraud.dto;
+package antifraud.dto.userDTOs;
 
 import antifraud.utils.Operation;
 import antifraud.validation.ValidEnum;
 import lombok.Data;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 
 @Data
 public class UserAccessDTO {
@@ -10,6 +13,8 @@ public class UserAccessDTO {
     @ValidEnum(clazz = Operation.class, message = "Operation is invalid.")
     private String operation;
 
+    @NotEmpty(message = "Username mustn't be empty")
+    @NotNull(message = "Username mustn't be null")
     private String username;
 
 }
--- src/antifraud/dto/UserRoleDTO.java
+++ src/antifraud/dto/UserRoleDTO.java
@@ -1,8 +1,11 @@
-package antifraud.dto;
+package antifraud.dto.userDTOs;
 
 import antifraud.utils.Role;
 import antifraud.validation.ValidEnum;
 import lombok.Data;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 
 @Data
 public class UserRoleDTO {
@@ -10,6 +13,8 @@ public class UserRoleDTO {
     @ValidEnum(clazz = Role.class, message = "Role is invalid.")
     private String role;
 
+    @NotEmpty(message = "Username mustn't be empty")
+    @NotNull(message = "Username mustn't be null")
     private String username;
 
 }
